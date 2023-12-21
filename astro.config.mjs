729f2732ef2c1a3f3cfc57c865b6a7d1c8c09b88
Alexander <soportexocialex@gmail.com>
2023-12-21 14:39:05 -0400

    Add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+//import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  //adapter: vercel(),
   site: 'https://xocialex.github.io',
-  base: '/Astro-WP-REST-API',
+  base: '/',
 });
