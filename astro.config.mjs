c2b2ef56ac1873a8dce72f988ed523aee6267f2e
Alexander <soportexocialex@gmail.com>
2023-12-21 14:57:30 -0400

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 
 //import vercel from "@astrojs/vercel/serverless";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   //adapter: vercel(),
   site: 'https://xocialex.github.io',
   base: '/',
+  adapter: node({
+    mode: "standalone"
+  })
 });
